6596dcb4d7112cb6d4b78f1df093c7d8159b122a
Marko Bandzo <marko_bandzo@hotmail.com>
2018-01-02 14:49:45 +1300

    Add files via upload

--- deque1.cpp
+++ deque1.cpp
@@ -10,20 +10,39 @@
  */
 #include <iostream>
 #include <deque>
+#include <string>
+#include <algorithm>
+#include <iterator>
 using namespace std;
 
 int main()
 {
-    deque<float> coll;    // deque container for floating-point elements
+    // create empty deque of strings
+    deque<string> coll;
 
-    // insert elements from 1.1 to 6.6 each at the front
-    for (int i=1; i<=6; ++i) {
-        coll.push_front(i*1.1);      // insert at the front
-    }
+    // insert several elements
+    coll.assign (3, string("string"));
+    coll.push_back ("last string");
+    coll.push_front ("first string");
 
-    // print all elements followed by a space
-    for (int i=0; i<coll.size(); ++i) {
-        cout << coll[i] << ' ';
-    }
+    // print elements separated by newlines
+    copy (coll.begin(), coll.end(),
+          ostream_iterator<string>(cout,"\n"));
     cout << endl;
+
+    // remove first and last element
+    coll.pop_front();
+    coll.pop_back();
+
+    // insert ``another'' into every element but the first
+    for (unsigned i=1; i<coll.size(); ++i) {
+        coll[i] = "another " + coll[i];
+    }
+
+    // change size to four elements
+    coll.resize (4, "resized string");
+
+    // print elements separated by newlines
+    copy (coll.begin(), coll.end(),
+          ostream_iterator<string>(cout,"\n"));
 }
